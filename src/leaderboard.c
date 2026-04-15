ecc5159151d75da123d1fb1b12d93fc8865438fd
PaulMick <paulwmick@gmail.com>
2026-04-14 21:48:49 -0400

    working on eeprom

--- src/leaderboard.c
+++ src/leaderboard.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "hardware/gpio.h"
 #include "hardware/i2c.h"
 
-#define PIN_SDA 6
-#define PIN_SCL 7
+#define MASTER_SDA 6
+#define MASTER_SCL 7
+#define I2C_BAUDRATE 100000
 
 void init_leaderboard() {
+    gpio_init(MASTER_SDA);
+    gpio_set_function(MASTER_SDA, GPIO_FUNC_I2C);
+    gpio_pull_up(MASTER_SDA);
+    gpio_init(MASTER_SCL);
+    gpio_set_function(MASTER_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(MASTER_SCL);
+
+    i2c_init(i2c1, I2C_BAUDRATE);
+
+    
+}
+
+void eeprom_write(uint16_t eeprom_addr, void *read_addr, uint32_t len_bytes) {
     
 }
--- src/leaderboard.h
+++ src/leaderboard.h
@@ -1,6 +1,9 @@
 #ifndef LEADERBOARD_H
 #define LEADERBOARD_H
 
+#include <stdint.h>
+
 void init_leaderboard();
+void eeprom_write(uint16_t eeprom_addr, void *read_addr, uint32_t len_bytes);
 
 #endif
