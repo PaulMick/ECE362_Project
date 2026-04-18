d12eddd0d27b1b0445218b6e165e37cf8fcc1de5
Payton Kapatoes <pkapatoe@purdue.edu>
2026-04-18 12:36:50 -0400

    Minor changes to unused leaderboard logic

--- src/leaderboard.c
+++ src/leaderboard.c
@@ -21,9 +21,11 @@ uint32_t end_game_timer() {
 
 void init_i2c() {
     gpio_set_function(20, GPIO_FUNC_I2C);
+    //gpio_pull_up(20);
     gpio_set_function(21, GPIO_FUNC_I2C);
+    //gpio_pull_up(21);
     //REMEMBER TO PUT PHYSICAL PULL UP RESISTORS IN THE CIRCUIT
-    i2c_init(i2c0, 400 * 1000); //400 kbs with fast mode
+    i2c_init(i2c0, 100 * 1000); //100 kbs with fast mode
 }
 
 /*
@@ -38,19 +40,19 @@ void eeprom_write(uint16_t loc, uint32_t time) {
     printf("Numbers: %d %d %d\n", buffer[0], buffer[1], buffer[2]);
     responses[0] = i2c_write_blocking(i2c0, 0x50, &buffer, 3, false);
     printf("Error: %d\n", responses[0]);
-    sleep_ms(4);
+    //sleep_ms(4);
 
     buffer[2] = (time & 0xFF0000) >> 16;
     printf("Numbers: %d %d %d\n", buffer[0], buffer[1], buffer[2]);
     responses[1] = i2c_write_blocking(i2c0, 0x50, &buffer, 3, false);
     printf("Error: %d\n", responses[1]);
-    sleep_ms(4);
+    //sleep_ms(4);
 
     buffer[2] = (time & 0xFF00) >> 8;
     printf("Numbers: %d %d %d\n", buffer[0], buffer[1], buffer[2]);
     responses[2] = i2c_write_blocking(i2c0, 0x50, &buffer, 3, false);
     printf("Error: %d\n", responses[2]);
-    sleep_ms(4);
+    //sleep_ms(4);
 
     buffer[2] = (time & 0xFF);
     printf("Numbers: %d %d %d\n", buffer[0], buffer[1], buffer[2]);
@@ -81,10 +83,10 @@ void eeprom_read(uint16_t loc, uint32_t * data_read, uint8_t length) {
     printf("READ\n");
     uint8_t buffer [2] = {(loc & 0xFF00) >> 8, loc & 0xFF};
     printf("Numbers: %d %d\n", buffer[0], buffer[1]);
-    sleep_ms(4);
-    int resp1 = i2c_write_blocking(i2c0, 0x50, &buffer, 2, false);
+    //sleep_ms(4);
+    int resp1 = i2c_write_blocking(i2c0, 0x50, &buffer, 2, true);
     printf("Error: %d\n", resp1);
-    sleep_ms(4);
+    //sleep_ms(4);
 
     if(resp1 != PICO_ERROR_GENERIC) {
         int resp2 = i2c_read_blocking(i2c0, 0x50, &data_read, 4, false);
