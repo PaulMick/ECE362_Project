8ed10a9a867a6410388bea58c133891c940a2ec1
Kabir Ray Malik <kabirraymalik@gmail.com>
2026-04-18 16:17:24 -0400

    fixed the button issue hopefully

--- src/inputs.c
+++ src/inputs.c
@@ -3,10 +3,10 @@
 #include "hardware/adc.h"
 #include "inputs.h"
 
-#define PADS_ISO      (1u << 8)
-#define PADS_IE       (1u << 6)
-#define PADS_PUE      (1u << 3)
-#define PADS_PDE      (1u << 2)
+#define PADS_ISO (1u << 8)
+#define PADS_IE (1u << 6)
+#define PADS_PUE (1u << 3)
+#define PADS_PDE (1u << 2)
 
 //input state stored here
 static bool btn_prev = false;
@@ -14,13 +14,8 @@ static bool btn_edge = false;
 static uint16_t pot_val = 0;
 
 int init_inputs(void) {
-    uint32_t mask = 1u << (START_BUTTON_PIN & 0x1fu); // select start btn (44 defined in header)
-    sio_hw->gpio_oe_clr = mask; // set input mode
-    io_bank0_hw->io[START_BUTTON_PIN].ctrl = GPIO_FUNC_SIO << IO_BANK0_GPIO0_CTRL_FUNCSEL_LSB; // select SIO func
-    hw_clear_bits(&pads_bank0_hw->io[START_BUTTON_PIN], PADS_BANK0_GPIO0_ISO_BITS); // remove pad isolation latch
-    hw_set_bits(&pads_bank0_hw->io[START_BUTTON_PIN], PADS_BANK0_GPIO0_IE_BITS); // input buffer on
-    hw_set_bits(&pads_bank0_hw->io[START_BUTTON_PIN], PADS_BANK0_GPIO0_PUE_BITS); // pull up 1, pull down 0
-    hw_clear_bits(&pads_bank0_hw->io[START_BUTTON_PIN], PADS_BANK0_GPIO0_PDE_BITS);
+    gpio_init(START_BUTTON_PIN);
+    gpio_set_dir(START_BUTTON_PIN, GPIO_IN);
 
     // pot on gpio45, adc channel 5
     reset_unreset_block_num_wait_blocking(RESET_ADC);
@@ -45,8 +40,7 @@ static uint16_t read_adc(void) {
 
 int update_inputs(void) {
     // detect button posedge
-    uint32_t input_pins = sio_hw->gpio_in;
-    bool btn_curr = (input_pins & (1u << (START_BUTTON_PIN & 0x1fu))) == 0;
+    bool btn_curr = gpio_get(START_BUTTON_PIN); // active-high: HIGH when pressed
     btn_edge = !btn_prev && btn_curr;
     btn_prev = btn_curr;
 
